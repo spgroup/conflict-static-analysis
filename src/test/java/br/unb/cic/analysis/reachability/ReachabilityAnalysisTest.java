2cede8eb2a9ff7f0d211ea8ccd19f51a8f850ffd
Rodrigo Bonifácio <rbonifacio123@gmail.com>
2018-12-07 15:04:52 +0100

    Test case for interprocedural different classes

--- src/test/java/br/unb/cic/analysis/reachability/ReachabilityAnalysisTest.java
+++ src/test/java/br/unb/cic/analysis/reachability/ReachabilityAnalysisTest.java
@@ -5,10 +5,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import br.unb.cic.analysis.model.Pair;
-import br.unb.cic.analysis.model.Statement;
-import br.unb.cic.analysis.reachability.ReachabilityAnalysis;
-import org.jgrapht.GraphPath;
-import org.jgrapht.graph.DefaultEdge;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -19,6 +15,7 @@ import soot.Transform;
 public class ReachabilityAnalysisTest {
 	
 	private ReachabilityAnalysis analysis;
+	private ReachabilityAnalysis interproceduralDifferentClasses;
 	private ReachabilityAnalysis intraprocedural;
 
 
@@ -48,7 +45,7 @@ public class ReachabilityAnalysisTest {
 				List<Pair<String, List<Integer>>> res = new ArrayList<>();
 				List<Integer> lines = new ArrayList<>();
 				lines.add(9);
-				res.add(new Pair("br.unb.cic.analysis.samples.InterproceduralTestCase", lines));
+				res.add(new Pair("br.unb.cic.analysis.samples.InterproceduralTestCaseSameClass", lines));
 				return res;
 			}
 			@Override
@@ -58,7 +55,26 @@ public class ReachabilityAnalysisTest {
 				lines.add(19);
 				lines.add(21);
 
-				res.add(new Pair("br.unb.cic.analysis.samples.InterproceduralTestCase", lines));
+				res.add(new Pair("br.unb.cic.analysis.samples.InterproceduralTestCaseSameClass", lines));
+				return res;
+			}
+		};
+		interproceduralDifferentClasses = new ReachabilityAnalysis() {
+			@Override
+			protected List<Pair<String, List<Integer>>> sourceDefinitions() {
+				List<Pair<String, List<Integer>>> res = new ArrayList<>();
+				List<Integer> lines = new ArrayList<>();
+				lines.add(9);
+				res.add(new Pair("br.unb.cic.analysis.samples.InterproceduralTestCaseDifferentClasses", lines));
+				return res;
+			}
+			@Override
+			protected List<Pair<String, List<Integer>>> sinkDefinitions() {
+				List<Pair<String, List<Integer>>> res = new ArrayList<>();
+				List<Integer> lines = new ArrayList<>();
+				lines.add(26);
+
+				res.add(new Pair("br.unb.cic.analysis.samples.NotRelevant", lines));
 				return res;
 			}
 		};
@@ -79,7 +95,8 @@ public class ReachabilityAnalysisTest {
 				return res;
 			}
 		};
-		PackManager.v().getPack("wjtp").add(new Transform("wjtp.reachability", analysis));
+		PackManager.v().getPack("wjtp").add(new Transform("wjtp.analysis", analysis));
+		PackManager.v().getPack("wjtp").add(new Transform("wjtp.interproceduralDifferentClasses", interproceduralDifferentClasses));
 		PackManager.v().getPack("wjtp").add(new Transform("wjtp.intraprocedural", intraprocedural));
 		soot.Main.main(new String[] {"-w", "-allow-phantom-refs", "-f", "J", "-keep-line-number", "-process-dir", "target/test-classes/"});
 	}
@@ -90,6 +107,8 @@ public class ReachabilityAnalysisTest {
 		Assert.assertEquals(2, analysis.getPaths().size());
 		Assert.assertNotNull(intraprocedural.getPaths());
 		Assert.assertEquals(1, intraprocedural.getPaths().size());
+		Assert.assertNotNull(interproceduralDifferentClasses.getPaths());
+		Assert.assertEquals(1, interproceduralDifferentClasses.getPaths().size());
 	}
 	
 
