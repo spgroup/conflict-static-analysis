ef9c04f1913b02271083dbd2104ca2eb95d777c3
Barbosa <mbo2@cin.ufpe.br>
2020-06-30 18:59:15 -0300

    add new test class and sample

---
+++ src/test/java/br/unb/cic/analysis/df/OverridingAssignmentAnalysisArraysConflictTest2.java
@@ -0,0 +1,64 @@
+package br.unb.cic.analysis.df;
+
+import br.unb.cic.analysis.AbstractMergeConflictDefinition;
+import br.unb.cic.analysis.SootWrapper;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import soot.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class OverridingAssignmentAnalysisArraysConflictTest2 {
+
+    private OverridingAssignmentAnalysis analysis;
+
+    @Before
+    public void configure() {
+        G.reset();
+        Collector.instance().clear();
+
+        AbstractMergeConflictDefinition definition = new AbstractMergeConflictDefinition() {
+            @Override
+            protected Map<String, List<Integer>> sourceDefinitions() {
+                Map<String, List<Integer>> res = new HashMap<>();
+                List<Integer> lines = new ArrayList<>();
+                lines.add(3);    //left
+                lines.add(12);    //left
+                res.put("br.unb.cic.analysis.samples.OverridingAssignmentArraysSample2", lines);
+                return res;
+            }
+
+            @Override
+            protected Map<String, List<Integer>> sinkDefinitions() {
+                Map<String, List<Integer>> res = new HashMap<>();
+                List<Integer> lines = new ArrayList<>();
+                lines.add(10);    //right
+                lines.add(13);    //right
+                res.put("br.unb.cic.analysis.samples.OverridingAssignmentArraysSample2", lines);
+                return res;
+            }
+        };
+
+        PackManager.v().getPack("jtp").add(
+                new Transform("jtp.oneConflict", new BodyTransformer() {
+                    @Override
+                    protected void internalTransform(Body body, String phaseName, Map<String, String> options) {
+                        analysis = new OverridingAssignmentAnalysis(body, definition);
+                    }
+                }));
+        String cp = "target/test-classes";
+        String targetClass = "br.unb.cic.analysis.samples.OverridingAssignmentArraysSample2";
+        PhaseOptions.v().setPhaseOption("jb", "use-original-names:true");
+
+        SootWrapper.builder().withClassPath(cp).addClass(targetClass).build().execute();
+    }
+
+    @Test
+    public void testDataFlowAnalysisExpectingOneConflict() {
+        Assert.assertEquals(1, analysis.getConflicts().size());
+    }
+}
