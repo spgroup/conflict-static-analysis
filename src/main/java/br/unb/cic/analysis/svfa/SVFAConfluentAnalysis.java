a5d6f2f164dc0c54566cd86dd276e58a8dc50440
Rafael Mota <rma7@cin.ufpe.br>
2020-07-01 21:00:40 -0300

    SVFA Confluence basic functionality

--- src/main/java/br/unb/cic/analysis/svfa/SVFAConfluentAnalysis.java
+++ src/main/java/br/unb/cic/analysis/svfa/SVFAConfluentAnalysis.java
@@ -11,17 +11,25 @@ public class SVFAConfluentAnalysis {
 
     private String cp;
     private AbstractMergeConflictDefinition definition;
-    private Set<ConfluenceConflict> confluentFlows;
+    private Set<ConfluenceConflict> confluentFlows = new HashSet<>();
 
     SVFAConfluentAnalysis(String classPath, AbstractMergeConflictDefinition definition) {
         this.cp = classPath;
         this.definition = definition;
     }
 
-    public  Set<ConfluenceConflict> getConfluentConflicts() {
+    /**
+     * After the execute method has been called, it returns the confluent conflicts returned by the algorithm
+     * @return a set of confluence conflicts
+     */
+    public Set<ConfluenceConflict> getConfluentConflicts() {
         return confluentFlows;
     }
 
+    /**
+     * Executes both source -> base and sink -> base SVFA analysis intersects then populating
+     * the confluentFlows attribute with the results
+     */
     public void execute() {
         SVFAAnalysis sourceBaseAnalysis = sourceBaseAnalysis();
         sourceBaseAnalysis.buildSparseValueFlowGraph();
@@ -31,35 +39,48 @@ public class SVFAConfluentAnalysis {
         SVFAAnalysis sinkBaseAnalysis = sinkBaseAnalysis();
         sinkBaseAnalysis.buildSparseValueFlowGraph();
         System.out.println(sinkBaseAnalysis.svgToDotModel());
-        Set<List<Node>> sinkBasePaths = sourceBaseAnalysis.findSourceSinkPaths();
+        Set<List<Node>> sinkBasePaths = sinkBaseAnalysis.findSourceSinkPaths();
 
-        confluentFlows = getConfluenceConflicts(sourceBasePaths, sinkBasePaths);
+        confluentFlows = intersectPathsByLastNode(sourceBasePaths, sinkBasePaths);
     }
 
-    private Set<ConfluenceConflict> getConfluenceConflicts(Set<List<Node>> paths1, Set<List<Node>> paths2) {
+    /**
+     * Intersects the list of paths looking for paths that have the same last nodes
+     * also ignores redundant node (that represent different jimple lines but the same Java line)
+     * @param paths1 A set of lists of nodes with at least 2 nodes
+     * @param paths1 A set of lists of nodes with at least 2 nodes
+     * @return A set of confluence conflicts
+     */
+    private Set<ConfluenceConflict> intersectPathsByLastNode(Set<List<Node>> paths1, Set<List<Node>> paths2) {
         Map<Node, List<Node>> pathEndHash = new HashMap<>();
 
         for (List<Node> path: paths1) {
-            pathEndHash.put(getLastNode(path), path.subList(0, path.size() - 1));
+            pathEndHash.put(getLastNode(path), path);
         }
 
         Set<ConfluenceConflict> result = new HashSet<>();
         for (List<Node> path : paths2) {
             Node lastNode = getLastNode(path);
             if (pathEndHash.containsKey(lastNode)) {
-                ConfluenceConflict conflict = new ConfluenceConflict(pathEndHash.get(lastNode), path.subList(0, path.size() - 1), lastNode);
-                result.add(conflict);
+                result.add(new ConfluenceConflict(pathEndHash.get(lastNode), path));
             }
         }
 
         return result;
     }
 
+    /**
+     * @param path A list of nodes with at least 2 nodes
+     * @return The last node of the list
+     */
     private Node getLastNode(List<Node> path) {
         assert path.size() > 1; // assume that all paths have at least one source and one sink
         return path.get(path.size() - 1);
     }
 
+    /**
+     * @return A instance of a child class of the SVFAAnalysis class that redefine source and sink as source and base
+     */
     private SVFAAnalysis sourceBaseAnalysis() {
         return new SVFAAnalysis(this.cp, this.definition) {
 
@@ -75,6 +96,9 @@ public class SVFAConfluentAnalysis {
         };
     }
 
+    /**
+     * @return A instance of a child class of the SVFAAnalysis class that redefine source and sink as source and base
+     */
     private SVFAAnalysis sinkBaseAnalysis() {
         return new SVFAAnalysis(this.cp, this.definition) {
             @Override
--- src/test/java/br/unb/cic/analysis/svfa/SVFAConfluentAnalysisTest.java
+++ src/test/java/br/unb/cic/analysis/svfa/SVFAConfluentAnalysisTest.java
@@ -1,16 +1,15 @@
 package br.unb.cic.analysis.svfa;
 
 import br.unb.cic.analysis.AbstractMergeConflictDefinition;
+import br.unb.cic.analysis.model.Conflict;
 import br.unb.cic.soot.graph.Graph;
 import br.unb.cic.soot.graph.Node;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import scalax.collection.GraphBase;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class SVFAConfluentAnalysisTest {
 
@@ -42,7 +41,14 @@ public class SVFAConfluentAnalysisTest {
     }
 
     @Test
-    public void testSVFAnalysisExpectingOneConflict() {
+    public void testSVFAConfluentAnalysisExpectingOneConflict() {
         analysis.execute();
+        Set<ConfluenceConflict> conflicts = analysis.getConfluentConflicts();
+        Assert.assertEquals(conflicts.size(), 1);
+        ConfluenceConflict conflict = conflicts.iterator().next();
+
+        Assert.assertEquals(conflict.toString(),
+                "SOURCE=>BASE: (br.unb.cic.analysis.samples.DoubleSourceSample,<br.unb.cic.analysis.samples.DoubleSourceSample: void main(java.lang.String[])>,8) => (br.unb.cic.analysis.samples.DoubleSourceSample,<br.unb.cic.analysis.samples.DoubleSourceSample: void main(java.lang.String[])>,14)\n" +
+                        "SINK=>BASE: (br.unb.cic.analysis.samples.DoubleSourceSample,<br.unb.cic.analysis.samples.DoubleSourceSample: void main(java.lang.String[])>,12) => (br.unb.cic.analysis.samples.DoubleSourceSample,<br.unb.cic.analysis.samples.DoubleSourceSample: void main(java.lang.String[])>,14)");
     }
 }
