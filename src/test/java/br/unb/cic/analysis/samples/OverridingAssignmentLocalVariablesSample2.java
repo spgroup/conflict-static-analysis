09cda58fec3fad5c2ef9f02fd70ea9b6a63ae7b3
Barbosa <mbo2@cin.ufpe.br>
2020-07-02 15:30:36 -0300

    test: update OverridingAssignmentLocalVariablesSample and add OverridingAssignmentAnalysisLocalVariablesConflictTest2

--- src/test/java/br/unb/cic/analysis/samples/OverridingAssignmentLocalVariablesSample2.java
+++ src/test/java/br/unb/cic/analysis/samples/OverridingAssignmentLocalVariablesSample2.java
@@ -1,15 +1,15 @@
 package br.unb.cic.analysis.samples;
 
-public class OverridingAssignmentLocalVariablesSample {
+public class OverridingAssignmentLocalVariablesSample2 {
 
     public static void main(String[] args) {
         int x=0, y=0;
 
         x = 1;   // left
         y = 2;   // right
-        x = x+1; // base
-        y = 3;   // left
-        x = 4;   // right
+        x = 3;   // base
+        y = x+1; // left
+        x = 21;  // right
 
         System.out.println(x);
     }
