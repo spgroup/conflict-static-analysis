7e36e2a6be6844a806233ef6152dcd3525321b0b
Barbosa <mbo2@cin.ufpe.br>
2020-12-09 14:46:52 -0300

    test(#31): add Interprocedural Override Assignment Test Cases

---
+++ src/test/java/br/unb/cic/analysis/ioa/ClassFieldWithParameterNotConflictAnalysisTest.java
@@ -0,0 +1,62 @@
+package br.unb.cic.analysis.ioa;
+
+import br.unb.cic.analysis.AbstractMergeConflictDefinition;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import soot.G;
+import soot.PackManager;
+import soot.Transform;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Ignore
+public class ClassFieldWithParameterNotConflictAnalysisTest {
+
+    private InterproceduralOverrideAssignment analysis;
+
+    @Before
+    public void configure() {
+        G.reset();
+
+        AbstractMergeConflictDefinition definition = new AbstractMergeConflictDefinition() {
+            @Override
+            protected Map<String, List<Integer>> sourceDefinitions() {
+                Map<String, List<Integer>> res = new HashMap<>();
+                List<Integer> lines = new ArrayList<>();
+                lines.add(8);
+                res.put("br.unb.cic.analysis.samples.OverridingAssignmentClassFieldWithParameterNotConflictInterProceduralSample", lines);
+                return res;
+            }
+
+            @Override
+            protected Map<String, List<Integer>> sinkDefinitions() {
+                Map<String, List<Integer>> res = new HashMap<>();
+                List<Integer> lines = new ArrayList<>();
+                lines.add(9);
+                res.put("br.unb.cic.analysis.samples.OverridingAssignmentClassFieldWithParameterNotConflictInterProceduralSample", lines);
+                return res;
+            }
+        };
+
+        analysis = new InterproceduralOverrideAssignment(definition);
+
+        PackManager.v().getPack("wjtp").add(new Transform("wjtp.analysis", analysis));
+        soot.options.Options.v().setPhaseOption("cg.spark", "on");
+        soot.options.Options.v().setPhaseOption("cg.spark", "verbose:true");
+        //	PhaseOptions.v().setPhaseOption("jb", "use-original-names:true");
+
+        String testClasses = "target/test-classes/";
+        soot.Main.main(new String[]{"-w", "-allow-phantom-refs", "-f", "J", "-keep-line-number", "-process-dir", testClasses});
+    }
+
+    @Test
+    public void interproceduralOverridingAssignmentTest() {
+        Assert.assertEquals(0, analysis.getConflicts().size());
+    }
+
+}
