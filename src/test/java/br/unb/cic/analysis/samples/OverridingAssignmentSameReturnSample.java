7d11b1723a022826fd9c4cb6f8e4c6020a01e140
Galileu Santos <galilasmb@gmail.com>
2021-03-31 21:43:42 -0300

    Merge modifications

---
+++ src/test/java/br/unb/cic/analysis/samples/OverridingAssignmentSameReturnSample.java
@@ -0,0 +1,10 @@
+package br.unb.cic.analysis.samples;
+
+public class OverridingAssignmentSameReturnSample {
+
+    public int m(){
+        int x=3, y=7;
+
+        return x+y; //left, right
+    }
+}
