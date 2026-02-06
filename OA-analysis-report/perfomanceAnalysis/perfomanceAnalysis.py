3c44667a454fa132c58329703f87217f73f16d07
Pedro Patriota <pedroarthursp06@gmail.com>
2026-02-05 21:01:32 -0300

    renaming one analysis

---
+++ OA-analysis-report/perfomanceAnalysis/perfomanceAnalysis.py
@@ -0,0 +1,8 @@
+class PerformanceAnalysis:
+    def __init__(self, data):
+        self.data = data
+
+    def analyze(self):
+        # Placeholder for performance analysis logic
+        # This method would contain the actual analysis code
+        pass
