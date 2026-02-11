7c67d1f85061ef64b7113b95b4a271e87f96ea52
Pedro Patriota <pedroarthursp06@gmail.com>
2026-02-10 22:13:34 -0300

    adding perfomance data

--- OA-analysis-report/perfomanceAnalysis/perfomanceAnalysis.py
+++
@@ -1,8 +0,0 @@
-class PerformanceAnalysis:
-    def __init__(self, data):
-        self.data = data
-
-    def analyze(self):
-        # Placeholder for performance analysis logic
-        # This method would contain the actual analysis code
-        pass
